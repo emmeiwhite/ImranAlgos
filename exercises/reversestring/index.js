4a01ed62f88a2c7eb2d3fd1b190fdc51d557e094
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-11-27 03:27:21 +0530

    reverse string 3 methods done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  /* ---  method1 --- */
+  const strArray = Array.from(str);
+  let finalString1 = strArray.reverse().join("");
+  // return finalString1;
+
+  /* ---  method2 --- little too much though */
+  const strArray2 = [...str];
+  let reverseArray = [];
+
+  for (let i = strArray2.length - 1; i >= 0; i--) {
+    // console.log(strArray2[i]);
+    reverseArray.push(strArray2[i]);
+    // console.log(reverseArray);
+  }
+
+  //   return reverseArray.join("");
+
+  /* ---  method3 --- */
+  let strArray3 = str.split("");
+  let reverse = "";
+  for (let i = strArray3.length - 1; i >= 0; i--) {
+    reverse += strArray3[i];
+  }
+
+  return reverse;
+}
+
+// reverse("Life");
 
 module.exports = reverse;
