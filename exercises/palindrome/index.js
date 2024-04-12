3c71e22f15ee38bea5d629e3b64d55ed49dfd578
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-12 14:30:25 +0530

    Palindrome Done !!

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,15 +7,65 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+/* --- Method-1
 function palindrome(str) {
-  const reverse = [...str].reduce((acc, letter) => letter + acc, "");
-  console.log(str);
-  console.log(reverse);
-  if (str === reverse) {
+  let reverse = [...str].reduce((acc, curr) => {
+    return (acc = curr + acc);
+  }, "");
+
+  if (reverse === str) {
     return true;
   } else {
     return false;
   }
 }
+
+--- */
+
+/* --- Method-2
+function palindrome(str) {
+  const reverse = Array.from(str).reverse().join("");
+  if (reverse === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+--- */
+
+/* --- Method-3 
+function palindrome(str) {
+  let reverse = "";
+  for (let ch of str) {
+    reverse = ch + reverse;
+  }
+  return str === reverse;
+}
+--- */
+
+/* --- Method-4 (Traditional - Language Independent) --- */
+
+function palindrome(str) {
+  let strArr = [...str];
+
+  const length = strArr.length;
+
+  let loopUntil = Math.floor(length / 2); // We are looping these number of times only
+
+  let isPalindrome = true;
+  for (let i = 0; i < loopUntil; i++) {
+    debugger;
+    if (strArr[i] === strArr[length - i - 1]) {
+      debugger;
+    } else {
+      isPalindrome = false;
+      break;
+    }
+  }
+
+  return isPalindrome;
+}
+
+palindrome("abbaba");
 
 module.exports = palindrome;
